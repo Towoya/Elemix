5f62ab5f4322ab58e5fa139b90b25b6ac722d3d3
Dustin Gabriel O. Nicolas <107316417+Towoya@users.noreply.github.com>
2024-06-03 21:02:10 +0800

    Added Exit

---
+++ Assets/Scripts/General Scripts/ExitTrigger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitTrigger : MonoBehaviour
+{
+    public GameObject panel; // Assign your panel object in the Unity Editor
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            panel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+}
