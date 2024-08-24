13e9d860ad6d6cb96f7de1f3234124bf5b13a805
Dustin Gabriel O. Nicolas <107316417+Towoya@users.noreply.github.com>
2024-08-25 00:53:29 +0800

    k

--- Assets/Scripts/WhenClickedCategory.cs
+++ Assets/Scripts/WhenClickedCategory.cs
@@ -19,12 +19,6 @@ public class WhenClickedCategory : MonoBehaviour
         
     }
 
-    /**public void OnPointerClick()
-    {
-        Category.SetActive(false);
-        Category2.SetActive(false);
-    }**/
-
     public void disablecategorybtn()
     {
         GameObject[] categories = { Category, Category2, Category3, Category4, Category5, Category6, Category7, Category8, Category9 };
@@ -40,26 +34,5 @@ public class WhenClickedCategory : MonoBehaviour
                 category.SetActive(true);
             }
         }
-
-
-        /**if (Category.activeInHierarchy == true)
-            Category.SetActive(false);
-         
-        else
-            Category.SetActive(true);
-           
-        if (Category2.activeInHierarchy == true)
-            Category2.SetActive(false);
-
-        else Category2.SetActive(true);
-        
-        if (Category3.activeInHierarchy == true)
-            Category3.SetActive(false);
-
-        else Category3.SetActive(true);**/
-
-        /**Category.SetActive(true);
-        Category2.SetActive(false);
-        Category3.SetActive(false);**/
     }
 }
