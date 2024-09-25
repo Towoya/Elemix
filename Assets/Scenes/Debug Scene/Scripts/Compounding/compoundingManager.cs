3916ac70e7299c034541a13727165bbde1e47ed5
Discord615 <vanbeau1@gmail.com>
2024-09-25 23:00:48 +0800

    Added Base Compounding Scripts

---
+++ Assets/Scenes/Debug Scene/Scripts/Compounding/compoundingManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class compoundingManager : MonoBehaviour
+{
+    public static compoundingManager instance { get; private set; }
+
+    private void Awake() {
+        if (instance != null){
+            Debug.LogError("More than one instance of " + instance.name + " exists in current scene");
+            Destroy(this);
+        } else {
+            instance = this;
+        }
+    }
+
+    private void Update() {
+        slotClick();
+    }
+
+    private void slotClick(){
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.transform.GetComponent<compoundingSlots>() == null) return;
+                if (!Input.GetMouseButtonDown(0)) return;
+                
+                // TODO: Get elementBlock object from player and pass it as parameter in insertElement()
+                // hit.transform.GetComponent<compoundingSlots>().insertElement();
+            }
+    }
+}
--- Assets/Scenes/Debug Scene/Scripts/Compounding/compoundingSlots.cs
+++ Assets/Scenes/Debug Scene/Scripts/Compounding/compoundingSlots.cs
@@ -2,29 +2,25 @@ using UnityEngine;
 
 public class compoundingSlots : MonoBehaviour
 {
-    public char? molecularLetter { get; private set; }
-    private void OnMouseOver() {
-        if (Input.GetMouseButtonDown(0)){
-            Debug.Log("Clicked");
-            // Test
-            placeIntoSlot('a');
-            char? playerElement = PlayerManager.instance.playerHeldElement;
-            if (playerElement != null){
-                setMolecularLetter(playerElement);
-                // TODO: fill placeIntoSlot function which places the cube model of the player held element into the slot
-            }
-        }
+    [SerializeField] char? elementLetter = null;
+    [SerializeField] bool interactable;
+
+    public void insertElement(GameObject elementBlock){
+        if (!interactable) return;
+        if (elementLetter != null) return;
+        
+        char elementBlockLetter = elementBlock.GetComponent<elementBlock>().elementLetter;
+
+        setElementLetter(elementBlockLetter);
+        elementBlock.transform.SetParent(this.gameObject.transform);
+        elementBlock.transform.localPosition = Vector3.zero;
     }
 
-    private void setMolecularLetter(char? molecularLetter) {
-        this.molecularLetter = molecularLetter;
+    private void setElementLetter(char letter){
+        this.elementLetter = letter;
     }
 
-    // Test Variables
-    [SerializeField] GameObject testObject;
-
-    private void placeIntoSlot(char? molecularLetter) {
-        testObject.transform.SetParent(gameObject.transform);
-        testObject.transform.localPosition = Vector3.zero;
+    private void clearElementLetter(){
+        this.elementLetter = null;
     }
 }
